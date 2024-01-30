864024539c2f4322a93350c31a1624831e1616ca
Benjamin Lévy <benjamin.n.levy@wanadoo.fr>
2024-01-30 18:10:57 +0100

    la totale

--- source/sign-all+dmg-notarize+staple_mac-externals.zsh
+++ source/sign-all+dmg-notarize+staple_mac-externals.zsh
@@ -12,11 +12,11 @@ mv $(find ../externals -d 1 -iname '*.mxo') '../mac_externals'
 
 # remove quarantine flag & codesign each external
 echo $MyPassword | sudo -S xattr -r -d com.apple.quarantine $(find '../mac_externals' -d 1 -iname '*.mxo')
-#security find-certificate -a -c "$CerticateCommonName" -Z $HOME/Library/Keychains/login.keychain
+security find-certificate -a -c "$CerticateCommonName" -Z $HOME/Library/Keychains/login.keychain
 security find-identity -p codesigning $HOME/Library/Keychains/login.keychain
-#security unlock-keychain -p $MyPassword $HOME/Library/Keychains/login.keychain
-#security list-keychains -d user -s "$HOME/Library/Keychains/login.keychain-db"
-codesign --keychain $HOME/Library/Keychains/login.keychain --deep --timestamp --force -s $CerticateCommonName $(find '../mac_externals' -d 1 -iname '*.mxo')
+security list-keychains -d user -s "$HOME/Library/Keychains/login.keychain"
+security unlock-keychain -p $MyPassword $HOME/Library/Keychains/login.keychain
+codesign --keychain $HOME/Library/Keychains/login.keychain --deep --timestamp --force -s "$CerticateCommonName" $(find '../mac_externals' -d 1 -iname '*.mxo')
 
 # create dmg with the externals
 #hdiutil create ../mac_externals.notarized.dmg -fs HFS+ -srcfolder ../mac_externals -ov
