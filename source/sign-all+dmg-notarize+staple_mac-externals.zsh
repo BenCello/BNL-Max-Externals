5c29404aa20122f75a2012a5bfc0a5057e774a41
Benjamin Lévy <benjamin.n.levy@wanadoo.fr>
2024-01-30 15:53:40 +0100

    Replace CertificateCommonName by ID

--- source/sign-all+dmg-notarize+staple_mac-externals.zsh
+++ source/sign-all+dmg-notarize+staple_mac-externals.zsh
@@ -1,6 +1,6 @@
 #! /bin/zsh -
 
-#CerticateCommonName=""
+#SigningID=E001E13C2F610B59C3176470422F35264A51FF09
 #AppSpecificPasswordName=""
 
 # remove existing dmg
@@ -12,7 +12,7 @@ mv $(find ../externals -d 1 -iname '*.mxo') '../mac_externals'
 
 # remove quarantine flag & codesign each external
 echo $MyPassword | sudo -S xattr -r -d com.apple.quarantine $(find '../mac_externals' -d 1 -iname '*.mxo')
-codesign --deep --timestamp --force --keychain $HOME/Library/Keychains/login.keychain -s $CerticateCommonName $(find '../mac_externals' -d 1 -iname '*.mxo')
+codesign --deep --timestamp --force --keychain $HOME/Library/Keychains/login.keychain -s $SigningID $(find '../mac_externals' -d 1 -iname '*.mxo')
 
 # create dmg with the externals
 #hdiutil create ../mac_externals.notarized.dmg -fs HFS+ -srcfolder ../mac_externals -ov
