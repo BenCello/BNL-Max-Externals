496c9273e2457ac1ec4adae88bce01325fcc7c48
Benjamin Lévy <benjamin.n.levy@wanadoo.fr>
2024-01-30 10:51:44 +0100

    [Drone] sign release build

--- source/sign-all+dmg-notarize+staple_mac-externals.zsh
+++ source/sign-all+dmg-notarize+staple_mac-externals.zsh
@@ -1,5 +1,8 @@
 #! /bin/zsh -
 
+CerticateCommonName=""
+AppSpecificPasswordName=""
+
 # remove existing dmg
 rm -f -- ../mac_externals.notarized.dmg
 
@@ -9,12 +12,12 @@ mv $(find ../externals -d 1 -iname '*.mxo') '../mac_externals'
 
 # remove quarantine flag & codesign each external
 sudo xattr -r -d com.apple.quarantine $(find '../mac_externals' -d 1 -iname '*.mxo')
-codesign --deep --timestamp --force -s "Developer ID Application: Benjamin Levy (8RBSER49GJ)" $(find '../mac_externals' -d 1 -iname '*.mxo')
+codesign --deep --timestamp --force -s $CerticateCommonName $(find '../mac_externals' -d 1 -iname '*.mxo')
 
 # create dmg with the externals
 hdiutil create ../mac_externals.notarized.dmg -fs HFS+ -srcfolder ../mac_externals -ov
 # notarize & staple dmg
-xcrun notarytool submit ../mac_externals.notarized.dmg --keychain-profile "AppleDev_Notarize_BLevy" --wait
+xcrun notarytool submit ../mac_externals.notarized.dmg --keychain-profile $AppSpecificPasswordName --wait
 xcrun stapler staple ../mac_externals.notarized.dmg
 
 # cleanup
