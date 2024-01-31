c098ad523f94843c78fd084a7a991c9497a5020d
Benjamin Lévy <benjamin.n.levy@wanadoo.fr>
2024-01-31 15:25:54 +0100

    [Drone] correct pass & password

--- source/scripts/sign-all_mac-externals.zsh
+++ source/scripts/sign-all_mac-externals.zsh
@@ -6,7 +6,7 @@
 # Manual version
 #sudo xattr -r -d com.apple.quarantine $(find '../../externals' -d 1 -iname '*.mxo')
 # Drone version
-echo $MyPassword | sudo -S xattr -r -d com.apple.quarantine $(find '../externals' -d 1 -iname '*.mxo')
+echo $MyPassword | sudo -S xattr -r -d com.apple.quarantine $(find '../../externals' -d 1 -iname '*.mxo')
 
 if [[ -d ../../externals/bc.upnpc.mxo ]]; then
   codesign --verbose --deep --timestamp --force --sign "$CerticateCommonName" $(find '../../externals/bc.upnpc.mxo' -d 3 -type f -iname 'libminiupnpc.*')
